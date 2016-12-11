c4fc75d808d3b94c2d2fd897360527bea002a2a2
Sangam1 <shrestha.sangam1@gmail.com>
2016-12-11 11:50:26 -0700

    Attempting fix for CreativeTab

--- src/main/java/com/sangam1/InventoryRepair/Main.java
+++ src/main/java/com/sangam1/InventoryRepair/Main.java
@@ -23,7 +23,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = StartupRef.MODID, version = StartupRef.VERSION, updateJSON = StartupRef.UPDATEURL, name = StartupRef.MODID, guiFactory = StartupRef.GUIFACTORY)
 public class Main {
 	
-	public static final CreativeTabs sangam1ir = new CreativeTabs("sangam1ir"){
+	public static final CreativeTabs sangam1ir = new CreativeTabs("inventoryrepair:sangam1ir"){
 	    @Override public ItemStack getTabIconItem() {
 	        return new ItemStack(ItemsList.ItemAutoSmelter);
 	    }

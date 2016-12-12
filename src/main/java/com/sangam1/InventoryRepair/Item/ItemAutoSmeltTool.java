0f7d8e15c220b74ea11e5a8c91770aee239b8ad9
Sangam1 <shrestha.sangam1@gmail.com>
2016-12-11 18:48:55 -0700

    Prep for r1.11-004
    Looking good!

--- src/main/java/com/sangam1/InventoryRepair/Item/ItemAutoSmeltTool.java
+++ src/main/java/com/sangam1/InventoryRepair/Item/ItemAutoSmeltTool.java
@@ -2,7 +2,12 @@ package com.sangam1.InventoryRepair.Item;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemAutoSmeltTool extends ItemBase{
 	
@@ -13,13 +18,12 @@ public class ItemAutoSmeltTool extends ItemBase{
 		active = false;
 	}
 	
-	/* Doesn't work any more
-	public ItemStack onItemRightClick(ItemStack item, World world, EntityPlayer player)
-	{
-		System.out.println("Item clicked");
-		return item;		
-	}
-	*/
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World itemStackIn, EntityPlayer worldIn, EnumHand playerIn)
+    {
+		System.out.println("Click!");
+        return new ActionResult(EnumActionResult.PASS, worldIn.getHeldItem(playerIn));
+    }
 	
 	
 }

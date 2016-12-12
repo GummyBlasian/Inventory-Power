9199abf2294559989508efd48518e11103a7beb0
Sangam1 <shrestha.sangam1@gmail.com>
2016-12-11 19:57:16 -0700

    Prep for r1.11-004

--- src/main/java/com/sangam1/InventoryRepair/Item/ItemAutoSmeltTool.java
+++ src/main/java/com/sangam1/InventoryRepair/Item/ItemAutoSmeltTool.java
@@ -1,8 +1,14 @@
 package com.sangam1.InventoryRepair.Item;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
@@ -11,16 +17,42 @@ import net.minecraft.world.World;
 public class ItemAutoSmeltTool extends ItemBase {
 
 	private boolean active;
+	private int burnTimeLeft;
 
 	public ItemAutoSmeltTool(String name) {
 		super(name);
 		active = false;
+		burnTimeLeft = 0;
+		this.setMaxStackSize(1);
 	}
 
 	@Override
-	public void onUpdate(ItemStack par1ItemStack, World par2World, Entity par3Entity, int par4, boolean par5) {
-		if(active){
-			
+	public void onUpdate(ItemStack par1ItemStack, World world, Entity par3Entity, int par4, boolean par5) {
+		EntityPlayer player = (EntityPlayer) par3Entity;
+		if (player == null || world.isRemote)
+			return;
+		System.out.println("Hello");
+		InventoryPlayer inventory = player.inventory;
+		if (active) {
+			if (burnTimeLeft == 0) {
+				for (int i = 0; i < inventory.getSizeInventory(); i++) {
+					if (inventory.getStackInSlot(i).getItem() == Items.COAL) {
+
+						return;
+					}
+				}
+			}
+			for (int i = 0; i < inventory.getSizeInventory(); i++) {
+				if (inventory.getStackInSlot(i) == new ItemStack(Blocks.IRON_ORE)) {
+					ItemStack smelt = inventory.getStackInSlot(i);
+					inventory.removeStackFromSlot(i);
+					for(int a = 0; a < smelt.getItemDamage(); i ++){
+						inventory.addItemStackToInventory(FurnaceRecipes.instance().getSmeltingResult(smelt));
+					}
+					
+					return;
+				}
+			}
 		}
 	}
 

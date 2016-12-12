d260c48671f9f0131910eeec856fd573496ed5bb
Sangam1 <shrestha.sangam1@gmail.com>
2016-12-11 18:55:39 -0700

    prep for r1.11-004

--- src/main/java/com/sangam1/InventoryRepair/Item/ItemAutoSmeltTool.java
+++ src/main/java/com/sangam1/InventoryRepair/Item/ItemAutoSmeltTool.java
@@ -1,29 +1,38 @@
 package com.sangam1.InventoryRepair.Item;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class ItemAutoSmeltTool extends ItemBase{
-	
+public class ItemAutoSmeltTool extends ItemBase {
+
 	private boolean active;
 
 	public ItemAutoSmeltTool(String name) {
 		super(name);
 		active = false;
 	}
-	
+
 	@Override
-	public ActionResult<ItemStack> onItemRightClick(World itemStackIn, EntityPlayer worldIn, EnumHand playerIn)
-    {
+	public void onUpdate(ItemStack par1ItemStack, World par2World, Entity par3Entity, int par4, boolean par5) {
+		if(active){
+			
+		}
+	}
+
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World itemStackIn, EntityPlayer worldIn, EnumHand playerIn) {
 		System.out.println("Click!");
-        return new ActionResult(EnumActionResult.PASS, worldIn.getHeldItem(playerIn));
-    }
-	
-	
+		if (active) {
+			active = false;
+		} else {
+			active = true;
+		}
+		return new ActionResult(EnumActionResult.PASS, worldIn.getHeldItem(playerIn));
+	}
+
 }

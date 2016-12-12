9ce7199db065d393db310572380170af64742605
Sangam1 <shrestha.sangam1@gmail.com>
2016-12-11 18:33:12 -0700

    Prep for r1.11-004

--- src/main/java/com/sangam1/InventoryRepair/Item/ItemAutoSmeltTool.java
+++ src/main/java/com/sangam1/InventoryRepair/Item/ItemAutoSmeltTool.java
@@ -1,9 +1,25 @@
 package com.sangam1.InventoryRepair.Item;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
 public class ItemAutoSmeltTool extends ItemBase{
+	
+	private boolean active;
 
 	public ItemAutoSmeltTool(String name) {
 		super(name);
+		active = false;
 	}
-
+	
+	/* Doesn't work any more
+	public ItemStack onItemRightClick(ItemStack item, World world, EntityPlayer player)
+	{
+		System.out.println("Item clicked");
+		return item;		
+	}
+	*/
+	
+	
 }

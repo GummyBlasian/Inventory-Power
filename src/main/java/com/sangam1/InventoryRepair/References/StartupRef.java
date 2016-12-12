84989881937ebdd66662b8b475672134cf696ecd
Sangam1 <shrestha.sangam1@gmail.com>
2016-12-11 17:45:13 -0700

    Attempt fix

--- src/main/java/com/sangam1/InventoryRepair/References/StartupRef.java
+++ src/main/java/com/sangam1/InventoryRepair/References/StartupRef.java
@@ -6,7 +6,7 @@ import net.minecraftforge.common.config.Configuration;
 
 public class StartupRef {
 
-	public static final String MODID = "inventoryrepair";
+	public static final String MODID = "sangamir";
 	public static final String VERSION = "1.11.003";
 	public static final String UPDATEURL = "https://gist.githubusercontent.com/GummyBlasian/ba8b7c339c98d929559759fa569c6110/raw/1cb70595cb410a32ca9385212221c44919f4d040/InventoryRepairChangeLog.json";
 	public static final String GUIFACTORY = "com.sangam1.InventoryRepair.Config.GUIFactoryInventoryRepair";

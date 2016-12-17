9de279f552141c30be97391ac330e0b713c0f63b
Sangam1 <shrestha.sangam1@gmail.com>
2016-12-16 22:24:13 -0700

    Attempt fix
    r1.11-006 dev build.

--- src/main/java/com/sangam1/InventoryRepair/ConfigHandler.java
+++ src/main/java/com/sangam1/InventoryRepair/ConfigHandler.java
@@ -12,6 +12,8 @@ public class ConfigHandler {
     public static boolean enableBlockIdGUI;
     public static boolean enableItemIdGUI;
     public static boolean enableItemRepair;
+    public static boolean enablePortableFurnace;
+    public static boolean enableGoCraft;
 
     public static void init(File file) {
         config = new Configuration(file);
@@ -26,6 +28,8 @@ public class ConfigHandler {
         enableBlockIdGUI = config.getBoolean("enableBlockIdGUI", category, true, "Allows GUI for displaying what a player is looking at. Disable if you don't want the GUI.");
         enableItemIdGUI = config.getBoolean("enableItemIdGUI", category, true, "Allows GUI for displaying what a player is holding. Disable if you don't want the GUI.");
         enableItemRepair = config.getBoolean("enableItemRepair", category, true, "Allows item auto-repair. Disable if you don't want item auto-repair.");
+        enablePortableFurnace = config.getBoolean("enablePortableFurnace", category, true, "Allow portable furnace. Disable if you don't want portable furnace.");
+        enableGoCraft = config.getBoolean("enableGoCraft", category, true, "Allow portable crafting table. Disable if you don't want portable crafting table.");
 
         config.save();
     }

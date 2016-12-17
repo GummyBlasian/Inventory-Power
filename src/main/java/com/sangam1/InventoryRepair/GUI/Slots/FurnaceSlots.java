1ae9dddcad51972bcc78c72cd3129b02afdca4ba
Sangam1 <shrestha.sangam1@gmail.com>
2016-12-16 22:28:36 -0700

    Attempt fix

--- src/main/java/com/sangam1/InventoryRepair/GUI/Slots/FurnaceSlots.java
+++ src/main/java/com/sangam1/InventoryRepair/GUI/Slots/FurnaceSlots.java
@@ -10,23 +10,9 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class FurnaceSlots extends Slot {
-	/** The index of the slot in the inventory. */
-	private final int slotIndex;
-	/** The inventory we want to extract a slot from. */
-	public final IInventory inventory;
-	/** the id of the slot(also the index in the inventory arraylist) */
-	public int slotNumber;
-	/** display position of the inventory slot on the screen x axis */
-	public int xDisplayPosition;
-	/** display position of the inventory slot on the screen y axis */
-	public int yDisplayPosition;
 
 	public FurnaceSlots(IInventory inventoryIn, int index, int xPosition, int yPosition) {
 		super(inventoryIn, yPosition, yPosition, yPosition);
-		this.inventory = inventoryIn;
-		this.slotIndex = index;
-		this.xDisplayPosition = xPosition;
-		this.yDisplayPosition = yPosition;
 	}
 
 	/**

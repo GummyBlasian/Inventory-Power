e4ce1b6a6a4775b695afae9e0f7968d24530ae0f
Sangam1 <shrestha.sangam1@gmail.com>
2019-12-20 19:26:09 -0700

    attempted fix of auto repair

--- src/main/java/com/sangam1/InventoryRepair/Events/Server/Auto_Repair.java
+++ src/main/java/com/sangam1/InventoryRepair/Events/Server/Auto_Repair.java
@@ -7,27 +7,27 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 
 public class Auto_Repair {
 
 	private static ItemStack itemstack;
 	private static Item item;
-	private static int count = 0;
-	
 	private static PlayerInventory playerInv;
-	
+
 	private static Boolean can_repair = false;
 	private static int repair_slot;
 	private static ItemStack repair_with;
 	private static int repair_percentage;
-	
+
 	public static void check_repair() {
-		if(can_repair) {
-			Main.LOGGER.info("in");
-			itemstack = Armor_Durability.getHand_Icon();
+		//if(can_repair) {
+		//Main.LOGGER.info("lets try");
+		itemstack = Armor_Durability.getHand_Icon();
+		if(itemstack != null) {
 			item = itemstack.getItem();
 			playerInv= Minecraft.getInstance().player.inventory;
-		
+
 			int i = 0;
 			double max = itemstack.getMaxDamage();
 			double current = itemstack.getDamage();
@@ -35,45 +35,47 @@ public class Auto_Repair {
 			repair_percentage = (int) Math.rint(max * 0.1);
 			if(x > 0.1) {
 				for(i = 0; i < playerInv.getSizeInventory(); i ++) {
-					if(item.isRepairable(playerInv.getStackInSlot(i))) {
+					if(item.isRepairable(playerInv.getStackInSlot(i)) && playerInv.getStackInSlot(i) != new ItemStack(Items.AIR)) {
 						can_repair = true;
 						repair_slot = i;
-						repair_with = playerInv.getStackInSlot(i);
+						Main.LOGGER.info("a " + i);
+						Main.LOGGER.info("b " + (repair_with = playerInv.getStackInSlot(repair_slot)));
+						break;
 					}
 				}
-			} else {
-				can_repair = false;
-				repair_slot = i;
-				repair_with = null;
-			}	
-		} else {
-			if(count == 50) {
-				can_repair = true;
+				if(can_repair) {
+					Main.LOGGER.info("c " + playerInv.getStackInSlot(repair_slot));
+					Main.LOGGER.info("d " + repair_slot);
+					Main.LOGGER.info("e " + repair_with);
+					return;
+				}
 			}
-			Main.LOGGER.info(count++);
 		}
+		can_repair = false;
+		repair_slot = 0;
+		repair_with = null;
 	}
-	
+
 	public static void set_can_repair(boolean set) {
 		can_repair = false;
 	}
-	
+
 	public static int repair_amount() {
 		return repair_percentage;
 	}
-	
+
 	public static PlayerInventory inventory() {
 		return playerInv;
 	}
-	
+
 	public static Boolean can_repair() {
 		return can_repair;
 	}
-	
+
 	public static int repair_slot() {
 		return repair_slot;
 	}
-	
+
 	public static ItemStack repair_with() {
 		return repair_with;
 	}
--- src/main/java/com/sangam1/InventoryRepair/Events/Server/Auto_Repair_Event.java
+++ src/main/java/com/sangam1/InventoryRepair/Events/Server/Auto_Repair_Event.java
@@ -3,29 +3,27 @@ package com.sangam1.InventoryRepair.Events.Server;
 import com.sangam1.InventoryRepair.Main;
 
 import net.minecraft.client.Minecraft;
-import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class Auto_Repair_Event {
+
+	@SubscribeEvent
+	public void onRenderTick(RenderGameOverlayEvent.Post event) {
+		if (Minecraft.getInstance().world.isRemote) {
+			//Main.LOGGER.info("sending check");
+			Auto_Repair.check_repair();
+			if(Auto_Repair.can_repair() && getRepair()) {
+				Main.LOGGER.info("Can!");
+				Auto_Repair.set_can_repair(false);
+				int damage = Auto_Repair.inventory().player.getHeldItemMainhand().getDamage();
+				Auto_Repair.inventory().player.getHeldItemMainhand().setDamage(damage - Auto_Repair.repair_amount());
+				Auto_Repair.inventory().decrStackSize(Auto_Repair.repair_slot(), 1);
+			}
+		}
+	}  
 	
-    @SubscribeEvent
-    public void onRenderTick(TickEvent.PlayerTickEvent event) {
-    	if(Minecraft.getInstance() != null) {
-    		if(Minecraft.getInstance().world != null) {
-    			if (!Minecraft.getInstance().world.isRemote) {
-    				Main.LOGGER.info("hi");
-    				Auto_Repair.check_repair();
-    				if(Auto_Repair.can_repair()) {
-    					Main.LOGGER.info("Can!");
-    					Auto_Repair.set_can_repair(false);
-    					int damage = Auto_Repair.inventory().player.getHeldItemMainhand().getDamage();
-    					Auto_Repair.inventory().player.getHeldItemMainhand().setDamage(damage + Auto_Repair.repair_amount());
-    					Auto_Repair.inventory().decrStackSize(Auto_Repair.repair_slot(), 1);
-    				} else {
-    					Main.LOGGER.info("Cant!");
-    				}
-    			}
-    		}
-    	}
-    }
+	private boolean getRepair() {
+		return Minecraft.getInstance().player.getHeldItemMainhand().getItem().isRepairable(Auto_Repair.repair_with());
+	}
 }

ad0ed15cd71a9d55ef814a861736eb994d3226c7
Sangam1 <shrestha.sangam1@gmail.com>
2019-12-27 12:36:37 -0700

    Improvements to Hand GUI

--- src/main/java/com/sangam1/InventoryRepair/GUI/Handlers/LargeArmorGUIHandler.java
+++ src/main/java/com/sangam1/InventoryRepair/GUI/Handlers/LargeArmorGUIHandler.java
@@ -11,12 +11,14 @@ public class LargeArmorGUIHandler {
 	public static void handGUI (FontRenderer textRenderer, Minecraft mc, int guiPosX, int guiPosY) {
 		ArmorDurability.getAll();
 		String armor_head = ArmorDurability.getHand();
-		String armor_head_durability = ArmorDurability.getHand_Durability();
-
-		int txtwidth_armor_head = mc.fontRenderer.getStringWidth(armor_head);
-		int txtwidth_armor_head_durability = mc.fontRenderer.getStringWidth(armor_head_durability);
-
+		String armor_head_durability = "";
 		if(ArmorDurability.getHand_Icon() != null) {	
+			if (ArmorDurability.getHand_Icon().getMaxDamage() != 0)
+				armor_head_durability = ArmorDurability.getHand_Durability();
+
+			int txtwidth_armor_head = mc.fontRenderer.getStringWidth(armor_head);
+			int txtwidth_armor_head_durability = mc.fontRenderer.getStringWidth(armor_head_durability);
+
 			mc.getItemRenderer().renderItemAndEffectIntoGUI(ArmorDurability.getHand_Icon(), guiPosX - txtwidth_armor_head - 20, guiPosY-35);
 			textRenderer.drawStringWithShadow(armor_head, guiPosX - txtwidth_armor_head, guiPosY - 32, TextFormatting.GOLD.getColor());
 			textRenderer.drawStringWithShadow(armor_head_durability, guiPosX - txtwidth_armor_head_durability, guiPosY - 22, TextFormatting.GOLD.getColor()); 
